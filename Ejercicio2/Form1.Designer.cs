8ddf0e7dcefc875345524294c595a6a8381061b1
usuario <matiaspierri60@gmail.com>
2022-05-04 22:08:08 -0300

    Codigo terminado

--- Ejercicio2/Form1.Designer.cs
+++ Ejercicio2/Form1.Designer.cs
@@ -100,12 +100,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(208, 389);
+            this.button1.Location = new System.Drawing.Point(187, 183);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 24);
             this.button1.TabIndex = 6;
             this.button1.Text = "Mostrar autos de la persona";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView4
             // 
--- Ejercicio2/Form1.cs
+++ Ejercicio2/Form1.cs
@@ -14,9 +14,13 @@ namespace Ejercicio2
     public partial class Form1 : Form
     {
         List<Persona> personaList = new List<Persona>();
+        List<Auto> autoList = new List<Auto>();
         public Form1()
         {
             InitializeComponent();
+
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -42,9 +46,38 @@ namespace Ejercicio2
 
         private void button5_Click(object sender, EventArgs e)
         {
-            string DNI = Interaction.InputBox
+            string DNI = Interaction.InputBox("DNI del titular");
 
-            Auto auto = new Auto(Patente, Marca, Modelo, Anio, Precio, Persona);
+            Persona persona = personaList.Find(p => p.DNI == DNI);
+
+
+            string Patente = Interaction.InputBox("Inserte Patente");
+            string Marca = Interaction.InputBox("Inserte Marca");
+            string Modelo = Interaction.InputBox("Inserte Modelo");
+            string Anio = Interaction.InputBox("Inserte Anio");
+            decimal Precio = Convert.ToDecimal(Interaction.InputBox("Inserte Precio"));
+
+
+
+
+            Auto auto = new Auto(Patente, Marca, Modelo, Anio, Precio, persona);
+
+            autoList.Add(auto);
+
+            Mostrar(dataGridView2, autoList);
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Persona persona = dataGridView1.SelectedRows[0].DataBoundItem as Persona;
+
+            List<Auto> autosPersona = autoList.FindAll(auto => auto.Persona.DNI == persona.DNI);
+
+            Mostrar(dataGridView3, autosPersona);
+
+           
         }
     }
 }
